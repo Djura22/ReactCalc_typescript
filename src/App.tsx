c71f1062d63d556561b1389d530f0ce6acb3a529
Djura22 <michaeliangroves@gmail.com>
2021-04-01 15:26:03 +0100

    Build: Comp: Calc Comp. Styles: styled-components

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,8 @@
 import React from 'react';
+import Calculator from './components/Calculator';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Calculator
-        </p>
-      </header>
-    </div>
-  );
+  return <Calculator></Calculator>
 }
 
 export default App;
