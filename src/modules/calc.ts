452ec06676eaff5832f3e8cd9470951504acdff5
Djura22 <michaeliangroves@gmail.com>
2021-04-08 11:58:48 +0100

    Build: OpsBuilder added to handle each requested Op. Logic for Number values built out, Operators next

--- src/modules/calc.ts
+++ src/modules/calc.ts
@@ -6,7 +6,7 @@
 
 export enum InputType {
   Number,
-  Operation
+  Operator
 }
 
 export enum OpType {
@@ -17,13 +17,44 @@ export enum OpType {
 
 export type CalcInput = 
   | {type: InputType.Number, value: number}
-  | {type: InputType.Operation; operation: OpType };
+  | {type: InputType.Operator; operator: OpType};
 
 export type CalcState = {
   displayValue: number;
 };
 
+/**
+ * Input [7, 5, +, 2, 5, =]
+ * Output [{+ 75}, {+ 25}, {=}]
+ * ------  Op Val  Op Val  Op
+ */
+
+export type Operation = {
+  operator: OpType;
+  value: number;
+}
+
+type OpsBuilder = {
+  operations: Operation[]
+  working: Omit<Operation, 'operator'> | null;
+}
+
+const getOperations = (inputs: Array<CalcInput>): Array<Operation> => {
+  const builder = inputs.reduce<OpsBuilder>((builder, input) => {
+
+    switch(input.type) {
+      case InputType.Number:
+        const prevValue = builder.working?.value || 0;
+        const newValue = prevValue * 10 + input.value;
+        return {...builder, working: {value: newValue}};
+    }
+
+  }, {operations: [], working: null} as OpsBuilder);
+}  
+
 const getState = (inputs: Array<CalcInput>): CalcState => {
+  const result = inputs.reduce();
+  
   return { displayValue: 0}
 }
 
